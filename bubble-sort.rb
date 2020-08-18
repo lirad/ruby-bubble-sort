5f9398b8fdfbe6f6687ca1121e67fe081a6d1cbe
Diego Lira <diegoalira@gmail.com>
2020-08-18 12:16:45 -0300

    Correct comments

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,17 +1,17 @@
-# Array to be sorted
+# Arrays to be sorted
 integer_arr = [4,3,78,2,0,2]
 string_arr = ["hi","hello","hey"]
 
 #Bubble sort
 def bubble_sort(array)
 times_loop = array.length-1
-  #Iterate troogh the array a Nnumber of times to make it sorted
+  #Iterate trhough the array a number of times to sort it
   times_loop.times {  
     #Swap counter to check if array is already sorted
     swap_counter = 0
     #Compare every item in array to the one next to it
     array.each_with_index do |item, index|
-      #Check if number is greater than the next number
+      #Check if the a number is greater than the b next number
       a = item
       b = array[index+1]
       is_greater = a <=> b
@@ -27,18 +27,19 @@ times_loop = array.length-1
   yield
 end
 
+#bubble_sort([4,3,78,2,0,2])
 bubble_sort(integer_arr) {puts "Sorted Integer: #{integer_arr}"}
 
 #Bubble Sort By
 def bubble_sort_by(array)
   times_loop = array.length-1
-    #Iterate troogh the array a Nnumber of times to make it sorted
+    #Iterate trhough the array a number of times to sort it
     times_loop.times {  
       #Swap counter to check if array is already sorted
       swap_counter = 0
       #Compare every item in array to the one next to it
       array.each_with_index do |item, index|
-        #Check if number is greater than the next number
+        #Check if a string is greater than the b string
         a = item
         b = array[index+1]
         is_lesser = b <=> a
@@ -54,5 +55,6 @@ def bubble_sort_by(array)
     yield
   end
   
+  #bubble_sort_by(["hi","hello","hey"])
   bubble_sort_by(string_arr) {puts "Sorted String: #{string_arr}"}
   
