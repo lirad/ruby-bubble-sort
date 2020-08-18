526f823b2868ffbee30aeddc3c5fe2844144327d
Diego Lira <diegoalira@gmail.com>
2020-08-18 12:14:00 -0300

    Bubble Sort By, Linters and ReadMe

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -2,7 +2,7 @@
 integer_arr = [4,3,78,2,0,2]
 string_arr = ["hi","hello","hey"]
 
-#Method to sort arrays
+#Bubble sort
 def bubble_sort(array)
 times_loop = array.length-1
   #Iterate troogh the array a Nnumber of times to make it sorted
@@ -27,4 +27,32 @@ times_loop = array.length-1
   yield
 end
 
-bubble_sort(integer_arr) {print "Sorted Array: #{arr}"}
+bubble_sort(integer_arr) {puts "Sorted Integer: #{integer_arr}"}
+
+#Bubble Sort By
+def bubble_sort_by(array)
+  times_loop = array.length-1
+    #Iterate troogh the array a Nnumber of times to make it sorted
+    times_loop.times {  
+      #Swap counter to check if array is already sorted
+      swap_counter = 0
+      #Compare every item in array to the one next to it
+      array.each_with_index do |item, index|
+        #Check if number is greater than the next number
+        a = item
+        b = array[index+1]
+        is_lesser = b <=> a
+        if is_lesser == 1 
+            array[index+1] = a
+            array[index] = b
+            swap_counter += 1          
+         end
+      end 
+      #Stop the loop if array is sorted
+      break if swap_counter == 0
+    }
+    yield
+  end
+  
+  bubble_sort_by(string_arr) {puts "Sorted String: #{string_arr}"}
+  
