5d29ebfda3017e407cdb67440714ae97d1560abe
AkintoyeOlamide <akintoyeolamide500@gmail.com>
2020-08-18 14:43:44 +0100

    iterate the array to work properly

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,30 +1,27 @@
 # Array to be sorted
-arr = [4,3,78,2,0,2]
+arr = [4,3,78,2,0,2,9,5,76,12]
 
 #Method to sort arrays
 def bubble_sort(array)
+times_loop = array.length-1
+ 
 
-  #Get the array values
-  array.each_with_index do |item, index|
-    #Check if number is greater than the next number
-    is_greater = array[index] <=> array[index+1]
-    first_number = array[index]
-    second_number = array[index+1]
-    case is_greater
-      when 1
-        #4
-        array[index+1] = item
-        array[index] = array[index+1]
-        puts "Is >"
-      when -1
-        puts "Is <" 
-      else 
-        puts "Is ="
-     end 
-  end
+  times_loop.times { #Get the array values
+    array.each_with_index do |item, index|
+      #Check if number is greater than the next number
+      first_number = item
+      second_number = array[index+1]
+      is_greater = first_number <=> second_number
+      case is_greater
+        when 1
+          array[index+1] = first_number
+          array[index] = second_number
 
-  print array
+        when -1
+        else 
+       end 
+    end 
+    print array}
+ 
 end
-
-
 bubble_sort(arr)
