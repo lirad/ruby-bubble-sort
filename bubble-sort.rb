2f508386cbbfd93ddb61ba6f378ef5e5b912dd8b
Diego Lira <diegoalira@gmail.com>
2020-08-18 11:08:17 -0300

    Finish Bubble Sort

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,27 +1,31 @@
 # Array to be sorted
-arr = [4,3,78,2,0,2,9,5,76,12]
+arr = [4,3,78,2,0,2,9,5,76,12,3,6,100,34,5]
+string_arr = ["hi","hello","hey"]
 
 #Method to sort arrays
 def bubble_sort(array)
 times_loop = array.length-1
- 
-
-  times_loop.times { #Get the array values
+  #iterate troogh the array a Number of time to make it sorted
+  #Try to break the loop if array is sorted
+  times_loop.times {  
+    #Swap counter to check if array is already sorted
+    swap_counter = 0
+    #Compared every item in array to each other
     array.each_with_index do |item, index|
       #Check if number is greater than the next number
-      first_number = item
-      second_number = array[index+1]
-      is_greater = first_number <=> second_number
-      case is_greater
-        when 1
-          array[index+1] = first_number
-          array[index] = second_number
-
-        when -1
-        else 
-       end 
+      a = item
+      b = array[index+1]
+      is_greater = a <=> b
+      if is_greater == 1 
+          array[index+1] = a
+          array[index] = b
+          swap_counter += 1          
+       end
     end 
-    print array}
- 
+    print array
+    #Stop the loop if array is sorted
+    break if swap_counter == 0
+  }
 end
+
 bubble_sort(arr)
