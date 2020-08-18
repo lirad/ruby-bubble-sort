e8fd0bf787c3e06928275b29e9155e227ec0b654
Diego Lira <diegoalira@gmail.com>
2020-08-18 11:25:04 -0300

    Add yield to Bubble Sort

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,16 +1,15 @@
 # Array to be sorted
-arr = [4,3,78,2,0,2,9,5,76,12,3,6,100,34,5]
+integer_arr = [4,3,78,2,0,2]
 string_arr = ["hi","hello","hey"]
 
 #Method to sort arrays
 def bubble_sort(array)
 times_loop = array.length-1
-  #iterate troogh the array a Number of time to make it sorted
-  #Try to break the loop if array is sorted
+  #Iterate troogh the array a Nnumber of times to make it sorted
   times_loop.times {  
     #Swap counter to check if array is already sorted
     swap_counter = 0
-    #Compared every item in array to each other
+    #Compare every item in array to the one next to it
     array.each_with_index do |item, index|
       #Check if number is greater than the next number
       a = item
@@ -22,10 +21,10 @@ times_loop = array.length-1
           swap_counter += 1          
        end
     end 
-    print array
     #Stop the loop if array is sorted
     break if swap_counter == 0
   }
+  yield
 end
 
-bubble_sort(arr)
+bubble_sort(integer_arr) {print "Sorted Array: #{arr}"}
