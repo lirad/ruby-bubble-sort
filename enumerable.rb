ce8cbbdeba054ab58ec7302b806c9e00b3b3f961
Diego Lira <diegoalira@gmail.com>
2020-08-19 12:33:10 -0300

    Revert "My each"
    This reverts commit 1c88292c031594ba146e8dd68dbf20dcf7b82535.

--- enumerable.rb
+++
@@ -1,9 +0,0 @@
-module Enumerable
-  def my_each(_array = nil)
-    return to_enum unless block_given?
-    Array(self).length.times do |i|
-      yield(Array(self)[i])
-    end
-    self
-  end
-end
