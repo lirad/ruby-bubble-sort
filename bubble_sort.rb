ef6d9d07757928aff876df3b9b4afb34a7f7f604
Diego Lira <diegoalira@gmail.com>
2020-08-20 05:04:24 -0300

    TSE corrections

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,3 @@
-integer_arr = [4, 3, 78, 2, 0, 2]
-string_arr = %w[hi hello hey]
-
 def bubble_sort(array)
   times_loop = array.length - 1
   times_loop.times do
@@ -17,30 +14,24 @@ def bubble_sort(array)
     end
     break if swap_counter.zero?
   end
-  yield
   array
 end
 
-bubble_sort(integer_arr) { puts "Sorted Integer: #{integer_arr}" }
+p bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(array)
-  times_loop = array.length - 1
-  times_loop.times do
+  a_length = array.length - 1
+  a_length.times do
     swap_counter = 0
-    array.each_with_index do |item, index|
-      a = item
-      b = array[index + 1]
-      is_lesser = b <=> a
-      next unless is_lesser == 1
-
-      array[index + 1] = a
-      array[index] = b
-      swap_counter += 1
+    (0..a_length - 1).each do |x|
+      if yield(array[x], array[x + 1]).positive?
+        array[x], array[x + 1] = array[x + 1], array[x]
+        swap_counter += 1
+      end
     end
-    break if swap_counter.zero?
+    break if swap_counter.positive? 
   end
-  yield
   array
 end
 
-bubble_sort_by(string_arr) { puts "Sorted String: #{string_arr}" }
+p bubble_sort_by(%w[hey hi hello]) { |left, right| left.length - right.length }
